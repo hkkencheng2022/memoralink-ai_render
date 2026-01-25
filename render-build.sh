6554b4e1067a9ad7f1fc16ec96185781ab9b99c2
ken cheng <164648495+hkkencheng2022@users.noreply.github.com>
2026-01-25 21:46:29 +0800

    Add files via upload

--- render-build.sh
+++ render-build.sh
@@ -2,11 +2,6 @@
 # exit on error
 set -o errexit
 
-# Only try to install node stuff if npm is available
-if command -v npm &> /dev/null
-then
-    npm install
-    npm run build
-fi
-
-pip install -r requirements.txt
+# Install dependencies and build the static site
+npm install
+npm run build
